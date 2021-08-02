bd4276ff7b972d0c44a9ff8c6b9b09988d741b65
Daesiker <eotlr680@naver.com>
2021-08-02 23:17:32 +0900

    SignUpView1

---
+++ TakeOff/SignUp/SignUp2View.swift
@@ -0,0 +1,40 @@
+//
+//  SignUp2View.swift
+//  TakeOff
+//
+//  Created by Jun on 2021/08/02.
+//
+
+import UIKit
+
+class SignUp2View: UIViewController {
+    
+    let user: User
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
