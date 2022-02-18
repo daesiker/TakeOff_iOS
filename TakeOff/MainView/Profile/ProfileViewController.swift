901956a43ff10527eda361c6cf5f0f07f73fa969
Daesiker <eotlr680@naver.com>
2022-02-18 18:19:51 +0900

    update Profileview

--- TakeOff/MainView/Profile/ProfileViewController.swift
+++ TakeOff/MainView/Profile/ProfileViewController.swift
@@ -15,14 +15,10 @@ import RxSwift
 class ProfileViewController: UIViewController {
     let vm = ProfileViewModel()
     
-    
+    let logOutBt = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain, target: self, action: nil)
     
     let exampleText = UITextField().then {
         $0.text = "ProfileViewController"
-    }
-    
-    let settingContainer = UIView().then {
-        $0.backgroundColor = .green
     }
     
     let userContainer = UIView().then {
@@ -32,23 +28,22 @@ class ProfileViewController: UIViewController {
         $0.backgroundColor = UIColor.yellow
     }
     
-    let userImage = UIImageView().then {
+    let profileImageView = UIImageView().then {
         $0.image = UIImage(named: "logo")
-        $0.layer.cornerRadius = 50
+        $0.layer.cornerRadius = 40
         $0.backgroundColor = UIColor.white   
         $0.layer.masksToBounds = true
     }
     
     
-    
-    let userLabel = UILabel().then {
+    let usernameLabel = UILabel().then {
         $0.text = User.loginedUser.name
-        $0.backgroundColor = UIColor.green   
+        $0.font = UIFont.boldSystemFont(ofSize: 14)
     }
     
     let postLabel = UILabel().then {
-        $0.text =  "0" //처음 0뜨는 거 막을려면 init으로 user 가져오거나 usermodel에 저장시켜놓기?
-        //  저장 시킬려면 로그인시 한번 저장 currentuser() 할때 저장값 최신화?
+        $0.text =  "0"
+        $0.numberOfLines = 0
         $0.textAlignment = .center
     }
     
@@ -72,29 +67,44 @@ class ProfileViewController: UIViewController {
 
 extension ProfileViewController {
     
+    
     private func setUI() {
         
         view.backgroundColor = .white
         
-        safeView.addSubview(settingContainer)
-        settingContainer.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-            $0.height.equalToSuperview().multipliedBy(0.05)
-        }
+        navigationItem.rightBarButtonItem = logOutBt
         
         safeView.addSubview(userContainer)
         userContainer.snp.makeConstraints {
-            $0.top.equalTo(settingContainer.snp.bottom)
-            $0.leading.trailing.equalToSuperview()
+            $0.leading.trailing.top.equalToSuperview()
             $0.height.equalToSuperview().multipliedBy(0.25)
+        }
+        
+        userContainer.addSubview(profileImageView)
+        profileImageView.snp.makeConstraints {
+            $0.leading.top.equalToSuperview().offset(12)
+            $0.width.height.equalTo(80)
+            
+        }
+        
+        userContainer.addSubview(usernameLabel)
+        usernameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(4)
+            $0.leading.equalToSuperview().offset(12)
         }
         
         safeView.addSubview(postContainer)
         postContainer.snp.makeConstraints {
             $0.top.equalTo(userContainer.snp.bottom)
             $0.leading.trailing.equalToSuperview()
-            $0.height.equalToSuperview().multipliedBy(0.7)
+            $0.height.equalToSuperview().multipliedBy(0.75)
         }
+        
+    }
+    
+    fileprivate func setupUserStackView() {
+        
+        
         
     }
     
