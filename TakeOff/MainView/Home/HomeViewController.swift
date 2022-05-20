dfced8c4e1acc5de38b748f2662b6f3419a72c27
Daesiker <eotlr680@naver.com>
2022-05-20 16:01:19 +0900

    Update HomeViewController.swift

--- TakeOff/MainView/Home/HomeViewController.swift
+++ TakeOff/MainView/Home/HomeViewController.swift
@@ -12,16 +12,48 @@ import Then
 
 class HomeViewController: UIViewController {
     
-    let exampleText = UITextField().then {
-        $0.text = "HomeViewController"
-    }
+    lazy var postCV:UICollectionView = {
+       let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+    }
+}
+
+extension HomeViewController {
+    
+    private func setUI() {
         view.backgroundColor = .white
-        view.addSubview(exampleText)
-        exampleText.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
+        safeView.addSubview(postCV)
+        postCV.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
     }
+    
+    private func setCV() {
+        self.postCV.dataSource = nil
+        self.postCV.delegate = nil
+        
+    }
+    
+    
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    
 }
