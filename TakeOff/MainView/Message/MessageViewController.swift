114966d59a85b70a350bb4b182920e02a90c6a70
Daesiker <eotlr680@naver.com>
2022-06-16 11:17:10 +0900

    Update MessageViewController.swift

--- TakeOff/MainView/Message/MessageViewController.swift
+++ TakeOff/MainView/Message/MessageViewController.swift
@@ -11,17 +11,41 @@ import SnapKit
 import Then
 import Firebase
 
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
+
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l > r
+    default:
+        return rhs < lhs
+    }
+}
+
+
 class MessageViewController: UITableViewController {
     
     let cellId = "cellId"
     var messages = [Message]()
     var messagesDictionary = [String: Message]()
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-        
-        
     }
     
     fileprivate func setUI() {
@@ -62,13 +86,45 @@ class MessageViewController: UITableViewController {
                     self.messagesDictionary[chatPartnerId] = message
                 }
                 
-                //self.attemptReloadOfTable()
+                self.attemptReloadOfTable()
             }
             
         }, withCancel: nil)
     }
     
+    fileprivate func attemptReloadOfTable() {
+        self.timer?.invalidate()
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.handleReloadTable), userInfo: nil, repeats: false)
+    }
     
+    @objc func handleReloadTable() {
+        self.messages = Array(self.messagesDictionary.values)
+        self.messages.sort(by: { (message1, message2) -> Bool in
+            
+            return message1.timestamp?.int32Value > message2.timestamp?.int32Value
+        })
+        
+        DispatchQueue.main.async(execute: {
+            self.tableView.reloadData()
+        })
+    }
+    
+    func fetchUserAndSetupNavBarTitle() {
+        // 현재 사용자의 uid를 가져옴
+        guard let uid = Auth.auth().currentUser?.uid else {
+            //for some reason uid = nil
+            return
+        }
+        
+        // Users 테이블에 있는 uid 테이블을 가져와서 안에 있는 데이터를 하나씩 실행
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let dictionary = snapshot.value as? [String: AnyObject] else {return}
+            let key = snapshot.key
+            let user = User(uid: key, dbInfo: dictionary)
+            self.setupNavBarWithUser(user)
+            
+        }, withCancel: nil)
+    }
     
     func setupNavBarWithUser(_ user: User) {
         messages.removeAll()
@@ -82,10 +138,67 @@ class MessageViewController: UITableViewController {
     }
     
     func showChatControllerForUser(_ user: User) {
-//        let chatLogController = ChatLogController(collectionViewLayout: UICollectionViewFlowLayout())
-//        chatLogController.user = user
-//        navigationController?.pushViewController(chatLogController, animated: true)
+        //        let chatLogController = ChatLogController(collectionViewLayout: UICollectionViewFlowLayout())
+        //        chatLogController.user = user
+        //        navigationController?.pushViewController(chatLogController, animated: true)
     }
     
 }
 
+extension MessageViewController {
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let uid = User.loginedUser.uid
+        
+        let message = self.messages[indexPath.row]
+        
+        if let chatPartherId = message.chatPartnerId() {
+            Database.database().reference().child("user-messages").child(uid).child(chatPartherId).removeValue { error, ref in
+                
+                if error != nil {
+                    print("Failed to delete message: ", error!)
+                }
+                
+                self.messagesDictionary.removeValue(forKey: chatPartherId)
+                
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserTableViewCell
+        let message = messages[indexPath.row]
+        cell.message = message
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let message = messages[indexPath.row]
+        
+        guard let chatPartnerId = message.chatPartnerId() else {
+            return
+        }
+        
+        let ref = Database.database().reference().child("users").child(chatPartnerId)
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let dictionary = snapshot.value as? [String: AnyObject] else {
+                return
+            }
+            
+            let key = snapshot.key
+            var user = User(uid: key, dbInfo: dictionary)
+            user.uid = chatPartnerId
+            self.showChatControllerForUser(user)
+            
+        }, withCancel: nil)
+    }
+    
+}
