c00aca8c4240f93e173b7d897e586531c6ca0dbc
Daesiker <eotlr680@naver.com>
2022-06-21 11:54:25 +0900

    Update NewMessageTableViewController.swift

--- TakeOff/MainView/Message/NewMessageTableViewController.swift
+++ TakeOff/MainView/Message/NewMessageTableViewController.swift
@@ -10,88 +10,85 @@ import Firebase
 import Then
 
 class NewMessageTableViewController: UITableViewController {
-
+    
     let cellId = "cellId"
     var filteredUsers:[User] = []
     var users:[User] = []
-    
+    var messageViewController:MessageViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUI()
+        fetchUsers()
     }
     
     fileprivate func setUI() {
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: <#T##Selector?#>)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         navigationItem.leftBarButtonItem?.tintColor = .orange
         tableView.register(UserTableViewCell.self, forCellReuseIdentifier: cellId)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
     }
+    
+    fileprivate func fetchUsers() {
+        let ref = Database.database().reference().child("users")
+        
+        ref.observeSingleEvent(of: .value) { snapshot in
+            guard let dictionaries = snapshot.value as? [String: Any] else { return }
+            
+            dictionaries.forEach { (key, value) in
+                
+                if key == User.loginedUser.uid {
+                    return
+                }
+                
+                guard let userDictionary = value as? [String: Any] else { return }
+                
+                let user = User(uid: key, dbInfo: userDictionary)
+                self.users.append(user)
+            }
+            
+            self.users.sort { u1, u2 in
+                return u1.name.compare(u2.name) == .orderedAscending
+            }
+            
+            self.filteredUsers = self.users
+            self.tableView.reloadData()
+            
+        }
+    }
+    
+    
+}
 
+extension NewMessageTableViewController {
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return filteredUsers.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserTableViewCell
+        
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dismiss(animated: true) {
+            let user = self.users[indexPath.row]
+            self.messageViewController?.showChatControllerForUser(user)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
+
